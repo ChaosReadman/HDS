ef0cd7f20bc2a3864768e08df3add0af5b1ba040
Takahiro Oku <pigment@mac.com>
2022-07-04 02:13:21 +0900

    お試し中

--- main.go
+++ main.go
@@ -7,6 +7,8 @@ import (
 	"os"
 	"strings"
 
+	"crypto/tls"
+
 	"github.com/comail/colog"
 	//	"github.com/pkg/profile"
 )
@@ -69,6 +71,7 @@ var pwd string
 
 func main() {
 	pwd, _ = os.Getwd()
+	// colog の設定
 	colog.SetDefaultLevel(colog.LDebug)
 	colog.SetMinLevel(colog.LDebug)
 	colog.SetFormatter(&colog.StdFormatter{
@@ -77,12 +80,60 @@ func main() {
 	})
 	colog.Register()
 
-	log.Println("info: Server Start....")
-	http.HandleFunc("/", controll)
+	/*
+		// p12を読む
+		// privとkeyを pkcs keystore からデコード
+		// デコードできない・・・
+		p12_data, err := os.ReadFile("auth/test.p12")
+		if err != nil {
+			log.Fatal(err)
+		}
 
-	err := http.ListenAndServeTLS(":8443", "auth/usercert.pem", "auth/userkey.pem", nil)
+		pkey, cer, err := pkcs12.Decode(p12_data, "abcdefg")
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		// 認証情報をtls構造体にセット
+		cert := tls.Certificate{}
+		cert.PrivateKey = pkey
+		cert.Certificate = [][]byte{cer.Raw}
+	*/
+
+	cer, err := tls.LoadX509KeyPair("auth/usercert.pem", "auth/userkey.pem")
 	if err != nil {
-		log.Printf("error : %s", err)
+		log.Println(err)
+		return
+	}
+
+	// ハンドラの設定
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", controll)
+
+	cfg := &tls.Config{
+		MinVersion:               tls.VersionTLS12,
+		CurvePreferences:         []tls.CurveID{tls.CurveP521, tls.CurveP384, tls.CurveP256},
+		PreferServerCipherSuites: true,
+		CipherSuites: []uint16{
+			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
+			tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_RSA_WITH_AES_256_CBC_SHA,
+		},
+		Certificates: []tls.Certificate{cer},
+	}
+
+	log.Println("info: Server Start....")
+
+	srv := &http.Server{
+		Addr:         ":443",
+		Handler:      mux,
+		TLSConfig:    cfg,
+		TLSNextProto: make(map[string]func(*http.Server, *tls.Conn, http.Handler), 0),
+	}
+	ListenErr := srv.ListenAndServeTLS()
+	if err != nil {
+		log.Printf("error : %s", ListenErr)
 	}
 	log.Printf("info: Server Started")
 }
